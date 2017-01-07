e1f51cf659a2f8b57c6b87f8f615dd8878ed09e6
Denis Lavrov <bahus.vel@gmail.com>
2017-01-07 15:26:05 +1345

    bit set seems to be working, need to check limits

--- src/main.c
+++ src/main.c
@@ -8,13 +8,34 @@ int main() {
 		perror("Failed to allocate set");
 		exit(-1);
 	}
-	if (!sb_set_set(set, 10)) {
+	if (!sb_set_set(set, 0)) {
 		perror("Failed to set value");
 		exit(-1);
 	}
-	if (!sb_set_set(set, 11)) {
+	if (!sb_set_set(set, 1)) {
 		perror("Failed to set value");
 		exit(-1);
 	}
+	if (!sb_set_set(set, 65536)) {
+		perror("Failed to set value");
+		exit(-1);
+	}
+	if (!sb_set_set(set, 65537)) {
+		perror("Failed to set value");
+		exit(-1);
+	}
+	if (!sb_set_test(set, 0)) {
+		printf("Failed to get %d\n", 0);
+	}
+	if (!sb_set_test(set, 1)) {
+		printf("Failed to get %d\n", 0);
+	}
+	if (!sb_set_test(set, 65536)) {
+		printf("Failed to get %d\n", 0);
+	}
+	if (!sb_set_test(set, 65537)) {
+		printf("Failed to get %d\n", 0);
+	}
+	printf("Blocks used %d\n", set->used_blocks);
 	return 0;
 }
